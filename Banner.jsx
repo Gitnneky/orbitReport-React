a53004523b2a134342153776dc9ca46cac14edb2
kristyneka <kristyneka@gmail.com>
2024-03-21 17:26:03 -0500

    Orbit

--- Banner.jsx
+++ Banner.jsx
@@ -1,12 +1,12 @@
 function Banner() {
-return (
-    <>
-<header style={{background:"blue"}}>
-<h1>"orbit report"</h1>
-</header>
-<p> Click on the buttons to see the satellites in that orbit type</p>
-</>
-);
+    return (
+        <>
+            <header style={{ background: "blue" }}>
+                <h1>Orbit report</h1>
+            </header>
+            <p> Click on the buttons to see the satellites in that orbit type</p>
+        </>
+    );
 }
 
 export default Banner;
